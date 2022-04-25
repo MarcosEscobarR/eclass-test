436cb40a2b691a58d7feddf024d8053e915f6dee
MarcosEscobarR <marcosmaer600@gmail.com>
2022-04-25 05:15:15 -0400

    Features

--- client/src/components/testList/TestList.tsx
+++ client/src/components/testList/TestList.tsx
@@ -1,19 +1,22 @@
-﻿interface IProps {
+﻿// @ts-ignore
+import styles from './TestList.module.scss'
+
+interface IProps {
     names: string[],
     order: 'ASC' | 'DSC'
 }
 function TestList({order, names}: IProps) {
-    return <>
-        {
-            order === "ASC" 
-            ? names.sort().map(p => (
-                <div key={p}>{p}</div>
+    return <div className={styles.container}>
+        <ul>{
+            order === "ASC"
+                ? names.sort().map(p => (
+                    <li key={p}>{p}</li>
                 ))
                 : names.sort().reverse().map(p => (
-                    <div key={p}>{p}</div>
+                    <li key={p}>{p}</li>
                 ))
-        }
-    </>
+        }</ul>
+    </div>
 }
 
 export default TestList;
--- client/src/shared/httpRequest.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : 'your api production url'
-});
